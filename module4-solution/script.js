b56ddd8fc75daf94e9e33d0a2853054dedf4da51
su-ux918 <62871586+su-ux918@users.noreply.github.com>
2020-06-19 21:34:34 +0600

    assignment3

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,11 +1,17 @@
-var name= prompt("Enter your Name:");
- var namec=name.split("")
+var name= prompt("Enter  Name:"); 
+// var name = ["yakov", "john", "smith","jabin"];
+// namec=name[0].split("");
+// namec=name[1].split("");
+// namec=name[2].split("");
+// namec=name[3].split("");
+
+// for (var index = 0; index < name.length; index++) {
+    
+var namec=name.split("");
 if (namec[0]=="J" || namec[0]=="j") {
-    document.write("Goodby  "+name);
+    console.log("Goodby  "+name);
     
 } else {
-    document.write("Hello  "+name);
+    console.log("Hello  "+name);
     // document.getElementById(name).innerHTML="Hello"+name;
-    
-    
 }
